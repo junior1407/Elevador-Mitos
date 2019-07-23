1de1cd72b3234a3f7d100372faec277844d41e5e
Valdir de Souza Junior <jr140797@gmail.com>
2019-07-23 12:47:06 -0300

    Add files via upload

--- Elevador-Mitos/main.asm
+++ Elevador-Mitos/main.asm
@@ -6,20 +6,113 @@
 	jmp handle_INT2
 .org OC1Aaddr
 	jmp OC1A_Interrupt
-
+.def contagem = r17
 .def temp = r16
+.def oldINT0 = r0
+.def oldINT2 = r2
+
+	ldi temp, 1
+	reti
+
+
+delay20ms:
+	push r22
+	push r21
+	push r20
+
+	ldi r22, byte3(16*1000*20 / 5)
+	ldi r21, high(16*1000*20 / 5)
+	ldi r20, low(16*1000*20 / 5)
+
+	subi r20,1
+	sbci r21,0
+	sbci r22,0
+	brcc pc-3
+
+	pop r20
+	pop r21
+	pop r22
+	ret
 
 
 
 handle_INT0:
-	ldi temp, 1
-	reti
+	.def temp2 = r18;
+	cli ; TODO: Só desligar essa interrupção
+	push temp2
+	; Fechar do Elevador = PB0; PCINT0
+	; Chamar 0 = PB2; PCINT2
+	; Chamar 1 = PB3; PCINT3 
+	; Chamar 2  = PB4; PCINT4
+	in temp2, PINB
+	call delay20ms;Debouncing
+	sbrc temp2,0
+	jmp botao_fechar_pressionado
+	sbrc temp2,2
+	jmp botao_chamar0_ext_pressionado
+	sbrc temp2,3
+	jmp botao_chamar1_ext_pressionado
+	sbrc temp2,4
+	jmp botao_chamar2_ext_pressionado
+	jmp end_handle_int0
+	botao_chamar0_ext_pressionado:
 
+		jmp end_handle_int0
+	botao_chamar1_ext_pressionado:
+
+		jmp end_handle_int0
+	botao_chamar2_ext_pressionado:
+
+		jmp end_handle_int0
+	botao_fechar_pressionado:
+
+		jmp end_handle_int0
+
+	end_handle_int0:
+	pop temp2
+	.undef temp2
+	sei; TODO: Só religar essa interrupção
+	reti
 
 
 handle_INT2:
-	ldi temp, 1
+	.def temp2 = r18;
+	cli ; TODO: Só desligar essa interrupção
+	push temp2
+	;0 do Elevador = PD4 ; PCINT20
+	;1 do Elevador = PD5 ; PCINT21
+	;2 do Elevador = PD6 ; PCINT22
+	;Abrir do Elevador = PD7; PCINT23
+	in temp2, PIND
+	call delay20ms;Debouncing
+	sbrc temp2,4
+	jmp botao_chamar0_in_pressionado
+	sbrc temp2,5
+	jmp botao_chamar1_in_pressionado
+	sbrc temp2,6
+	jmp botao_chamar2_in_pressionado
+	sbrc temp2,7
+	jmp botao_abrir_pressionado
+	jmp end_handle_int1
+	botao_chamar0_in_pressionado:
+
+		jmp end_handle_int1
+	botao_chamar1_in_pressionado:
+
+		jmp end_handle_int1
+	botao_chamar2_in_pressionado:
+
+		jmp end_handle_int1
+	botao_abrir_pressionado:
+
+		jmp end_handle_int1
+
+	end_handle_int1:
+	pop temp2
+	.undef temp2
+	sei; TODO: Só religar essa interrupção
 	reti
+
 
 OC1A_Interrupt:
 	ldi temp, 1
@@ -31,6 +124,7 @@ OC1A_Interrupt:
 ;1 do Elevador = PD5 ; PCINT21
 ;2 do Elevador = PD6 ; PCINT22
 ;Abrir do Elevador = PD7; PCINT23
+
 ; Fechar do Elevador = PB0; PCINT0
 ; Buzzer = PB1;  PCINT1
 ; Chamar 0 = PB2; PCINT2
@@ -41,6 +135,9 @@ OC1A_Interrupt:
 ;sbi PORTD, 2
 ;cbi PORTD, 2
 
+
+
+
 reset: 
 cli
 ;Pin change Interrupt (23:16) and (0:7)
@@ -49,6 +146,7 @@ sts PCICR, temp
  
 ; Enables PCINT 4 TO 0, but 1 (the buzzer).
 ldi temp, 0b00011101;
+;out PCMSK0, temp
 sts PCMSK0, temp 
 
 
@@ -89,6 +187,9 @@ ldi temp, low(RAMEND)
 out SPL, temp
 ldi temp, high(RAMEND)
 out SPH, temp
+
+;getInts current values
+;in 
 
 sei ;Enable Interrupts
 rjmp pc; Infinite Loop
